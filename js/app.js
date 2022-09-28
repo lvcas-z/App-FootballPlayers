3f40ff2f82c07e63de042ff1895e5b7d681e1e2a
Lucas Ivan Zelaya <lvcaszelaya@gmail.com>
2022-09-28 14:36:56 -0300

    cree la funcion getOption

--- js/app.js
+++ js/app.js
@@ -1,31 +1,40 @@
 const cardsContainer = document.getElementById('container');
 const template = document.getElementById('templateCard').content;
 const fragment = document.createDocumentFragment();
+const select = document.querySelector('#region');
+
 
 document.addEventListener('DOMContentLoaded',() => {
+    getOption()
     fetchData();
 })
 
+const getOption = () =>{
+    if (localStorage.getItem('option')) {
+        select.value = localStorage.getItem('option')
+    }
+    select.addEventListener('change',() => localStorage.setItem('option',select.value))
+}
+
 const fetchData = async () => {
     try {
-        const res = await fetch('../../prueba.json')
+        const res = await fetch('../../api.json')
         const data = await res.json();
-        console.log(data);
-        // cardBuilder(data);
+        cardBuilder(data)
     } catch (error) {
         console.log(error);
     }
 }
 
-// const cardBuilder = data => {
-//     data.forEach(player => {
-//         template.querySelector('img').setAttribute("src", player.image)
-//         template.querySelector('h4').textContent = player.name.toUpperCase()
-//         template.querySelector('.position').textContent = `Position: ${player.position.toUpperCase()}`
-//         template.querySelector('.country').textContent = `Country: ${player.country.toUpperCase()}`
-//         template.querySelector('.birth').textContent = `Date of Birth: ${player.birth}`
-//         const clone = template.cloneNode(true)
-//         fragment.appendChild(clone)
-//     })
-//     cardsContainer.appendChild(fragment)
-// }
+const cardBuilder = data => {
+    data.forEach(player => {
+        template.querySelector('img').setAttribute("src", player.image)
+        template.querySelector('h4').textContent = player.name.toUpperCase()
+        template.querySelector('.position').textContent = `Position: ${player.position.toUpperCase()}`
+        template.querySelector('.country').textContent = `Country: ${player.country.toUpperCase()}`
+        template.querySelector('.birth').textContent = `Date of Birth: ${player.birth}`
+        const clone = template.cloneNode(true)
+        fragment.appendChild(clone)
+    })
+    cardsContainer.appendChild(fragment)
+}
